235ed738751ec283ab04708bba51b39660a8ac81
josemorista <josemorista@id.uff.br>
2022-08-30 20:57:47 -0300

    chore: fly.io deploy

--- src/application/infra/cache/services/CacheService.mjs
+++ src/application/infra/cache/services/CacheService.mjs
@@ -1,15 +1,9 @@
 // @ts-check
-import { CacheService, DynamoDbCacheStrategy } from 'cache-service-lib';
+import { CacheService, MemCacheStrategy } from 'cache-service-lib';
 
 const cacheService = new CacheService();
-cacheService.registerStrategy('dynamodb', new DynamoDbCacheStrategy({}, {
-	cacheHashValue: '#CACHE',
-	hashAttribute: 'kind',
-	keyAttribute: 'key',
-	table: 'wedding_jose_mari',
-	ttlAttribute: 'ttl'
-}));
 
-export {
-	cacheService
-};
+const strategy = new MemCacheStrategy();
+cacheService.registerStrategy('mem', strategy);
+
+export { cacheService };
