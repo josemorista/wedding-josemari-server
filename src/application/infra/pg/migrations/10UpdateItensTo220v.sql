0bed42b618aab328a55335245a64708e7d3e2529
josemorista <josemorista@id.uff.br>
2022-11-18 22:49:54 -0300

    chore:10UpdateItensTo220v migration

---
+++ src/application/infra/pg/migrations/10UpdateItensTo220v.sql
@@ -0,0 +1,6 @@
+update item set name='Passadeira a vapor - 110v' where id=32;
+update item set name='Mixer Britânia - 110v' where id=33;
+delete from item where id=17;
+update item set suggested_seller='https://www.americanas.com.br/produto/7470828?pfm_carac=passadeira-a-vapor-lisser-cadence&pfm_index=1&pfm_page=search&pfm_pos=grid&pfm_type=search_page&offerId=5ce4316bf216c95bde89e906&buyboxToken=smartbuybox-acom-v2-427ae837-2265-4b3c-9600-814210f4ccff-2022-11-19%2001%3A41%3A50%200000&voltagem=110%20volts&condition=NEW'
+  where id=32;
+update item set suggested_seller='https://www.amazon.com.br/Mixer-velocidades-Bmx400-Preto-Brit%C3%A2nia/dp/B08YHR9C4S' where id=33;
