8dd64d9093a66a1549ba53bfa8c809c91dd49be7
josemorista <josemorista@id.uff.br>
2023-01-05 23:31:28 -0300

    fix: migration fix

--- src/application/infra/pg/migrations/11UpdateGifts.sql
+++ src/application/infra/pg/migrations/11UpdateGifts.sql
@@ -9,5 +9,5 @@ insert into item(id, name, picture, quantity_available_to_give, quantity_needed,
 insert into item(id, name, picture, quantity_available_to_give, quantity_needed, average_price, suggested_seller) values
 (40, 'Petisqueira Grande Ecokitchen', 'petisqueira.jpg', 1, 1, 59.99, 'https://www.amazon.com.br/Petisqueira-Ecokitchen-Mimo-Style-BM1418/dp/B077L7P4HF'),
 (41, 'Caminho de Mesa Raye 38 cm x 1,60 m', 'caminho.webp', 1, 1, 49.99, 'https://www.camicado.com.br/p/caminho-de-mesa-raye-38-cm-x-160-m-home-style/-/A-300049375-br.lc?sku=000000000000049375&utm_id=18288222626'),
-(42, 'Kit Potes de Plástico Herméticos Electrolux', 'potes.jpg', 1, 1, 81.90, 'https://www.amazon.com.br/Kit-Potes-para-Alimentos-Electrolux/dp/B0784CLYGQ')
-(43, 'Conjunto para Fondue Brinox Vermelho', 'fondue.jpg', 1, 1, 125.89, 'https://www.amazon.com.br/Conjunto-Fondue-Chocolate-Pecas-Brinox/dp/B07QQV4HVQ')
+(42, 'Kit Potes de Plástico Herméticos Electrolux', 'potes.jpg', 1, 1, 81.90, 'https://www.amazon.com.br/Kit-Potes-para-Alimentos-Electrolux/dp/B0784CLYGQ'),
+(43, 'Conjunto para Fondue Brinox Vermelho', 'fondue.jpg', 1, 1, 125.89, 'https://www.amazon.com.br/Conjunto-Fondue-Chocolate-Pecas-Brinox/dp/B07QQV4HVQ');
